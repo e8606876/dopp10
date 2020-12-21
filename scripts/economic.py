20b0be14bc2a9665327107c5ad5af7d947beda73
Hideosan500 <hideosan500@gmail.com>
2020-12-21 16:50:48 +0100

    add GDP data from world bank

---
+++ scripts/economic.py
@@ -0,0 +1,10 @@
+﻿import pandas as pd
+
+# load dataframe of whole world
+df = pd.read_csv('../data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389.csv',
+                 sep=',', skip_blank_lines=True, header=2)
+
+# for testing purposes
+a = df[df['Country Name'] == 'Austria']
+
+b = 0
