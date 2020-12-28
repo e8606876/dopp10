00388069c2e5967a173eec3516d50001ba182d77
Hideosan500 <hideosan500@gmail.com>
2020-12-28 16:17:24 +0100

    add GDP per capita and GDP per capita growth

--- scripts/load_data_economy.py
+++ scripts/load_data_economy.py
@@ -12,6 +12,8 @@ def load_economical_data():
     1: country code
     3..: features"""
 
+    dfs = []  # List of all dataframes.
+
     # load dataframe of GDP
     df_GDP = pd.read_csv('../data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389/'
                          'API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389.csv',
@@ -21,15 +23,71 @@ def load_economical_data():
 
     # melt and order to get in right format
     df_GDP = df_GDP.melt(id_vars=['country'], var_name='year', value_name='GDP')
+    df_GDP['year'] = df_GDP['year'].astype('int64')
     df_GDP = df_GDP[['year', 'country', 'GDP']]
     df_GDP.sort_values(['year', 'country'], inplace=True)
 
-    return df_GDP
+    dfs.append(df_GDP)
+
+    # load dataframe of GDP growth
+    df_GDP_growth = pd.read_csv('../data/API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_1836177/'
+                                'API_NY.GDP.MKTP.KD.ZG_DS2_en_csv_v2_1836177.csv',
+                                sep=',', skip_blank_lines=True, header=2)
+    df_GDP_growth.drop(['Country Name', 'Indicator Name', 'Indicator Code', 'Unnamed: 65'], axis=1, inplace=True)
+    df_GDP_growth.rename(columns={'Country Code': 'country'}, inplace=True)
+
+    # melt and order to get in right format
+    df_GDP_growth = df_GDP_growth.melt(id_vars=['country'], var_name='year', value_name='GDP growth')
+    df_GDP_growth['year'] = df_GDP_growth['year'].astype('int64')
+    df_GDP_growth = df_GDP_growth[['year', 'country', 'GDP growth']]
+    df_GDP_growth.sort_values(['year', 'country'], inplace=True)
+
+    dfs.append(df_GDP_growth)
+
+    # load dataframe of GDP per capita
+    df_GDP_per_capita = pd.read_csv('../data/API_NY.GDP.PCAP.CD_DS2_en_csv_v2_1740213/'
+                                    'API_NY.GDP.PCAP.CD_DS2_en_csv_v2_1740213.csv',
+                                    sep=',', skip_blank_lines=True, header=2)
+    df_GDP_per_capita.drop(['Country Name', 'Indicator Name', 'Indicator Code', 'Unnamed: 65'], axis=1, inplace=True)
+    df_GDP_per_capita.rename(columns={'Country Code': 'country'}, inplace=True)
+
+    # melt and order to get in right format
+    df_GDP_per_capita = df_GDP_per_capita.melt(id_vars=['country'], var_name='year', value_name='GDP per capita')
+    df_GDP_per_capita['year'] = df_GDP_per_capita['year'].astype('int64')
+    df_GDP_per_capita = df_GDP_per_capita[['year', 'country', 'GDP per capita']]
+    df_GDP_per_capita.sort_values(['year', 'country'], inplace=True)
+
+    dfs.append(df_GDP_per_capita)
+
+    # load dataframe of GDP per capita growth
+    df_GDP_per_capita_growth = pd.read_csv('../data/API_NY.GDP.PCAP.KD.ZG_DS2_en_csv_v2_1740284/'
+                                           'API_NY.GDP.PCAP.KD.ZG_DS2_en_csv_v2_1740284.csv',
+                                           sep=',', skip_blank_lines=True, header=2)
+    df_GDP_per_capita_growth.drop(['Country Name', 'Indicator Name', 'Indicator Code', 'Unnamed: 65'], axis=1,
+                                  inplace=True)
+    df_GDP_per_capita_growth.rename(columns={'Country Code': 'country'}, inplace=True)
+
+    # melt and order to get in right format
+    df_GDP_per_capita_growth = df_GDP_per_capita_growth.melt(id_vars=['country'], var_name='year',
+                                                             value_name='GDP per capita growth')
+    df_GDP_per_capita_growth['year'] = df_GDP_per_capita_growth['year'].astype('int64')
+    df_GDP_per_capita_growth = df_GDP_per_capita_growth[['year', 'country', 'GDP per capita growth']]
+    df_GDP_per_capita_growth.sort_values(['year', 'country'], inplace=True)
+
+    dfs.append(df_GDP_per_capita_growth)
+
+    # merge all dataframes
+    result = dfs[0]
+    for df in dfs[1:]:
+        result = result.merge(df, how='outer', on=['country', 'year'])
+
+    # todo set index
+
+    return result
 
 
 # test
 if __name__ == '__main__':
 
-    df = load_economical_data()
-
-    print(df.info())
+    dataframe = load_economical_data()
+    print(dataframe.info())
