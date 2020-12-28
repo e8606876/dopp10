81abb46cbcf25622590dcbab1f8988abeaff2dd8
Hideosan500 <hideosan500@gmail.com>
2020-12-28 15:19:19 +0100

    load GDP data

--- scripts/load_data_economy.py
+++ scripts/load_data_economy.py
@@ -1,10 +1,40 @@
 ﻿import pandas as pd
+import country_converter as coco
 
-# load dataframe of whole world
-df = pd.read_csv('../data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389.csv',
-                 sep=',', skip_blank_lines=True, header=2)
+converter = coco.CountryConverter()
 
-# for testing purposes
-a = df[df['Country Name'] == 'Austria']
 
-b = 0
+def load_economical_data():
+    """Load economical data into dataframe and return it.
+
+    Common data structure:
+    0: year
+    1: country code
+    3..: features"""
+
+    # load dataframe of GDP
+    df_GDP = pd.read_csv('../data/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389/'
+                         'API_NY.GDP.MKTP.CD_DS2_en_csv_v2_1740389.csv',
+                         sep=',', skip_blank_lines=True, header=2)
+    df_GDP.drop(['Country Name', 'Indicator Name',  'Indicator Code', 'Unnamed: 65'], axis=1, inplace=True)
+    df_GDP.rename(columns={'Country Code': 'country'}, inplace=True)
+
+    # melt and order to get in right format
+    df_GDP = df_GDP.melt(id_vars=['country'], var_name='year', value_name='GDP')
+    df_GDP = df_GDP[['year', 'country', 'GDP']]
+    df_GDP.sort_values(['year', 'country'], inplace=True)
+
+    return df_GDP
+
+
+# test
+if __name__ == '__main__':
+
+    df = load_economical_data()
+
+    print(df.info())
+
+
+
+
+
