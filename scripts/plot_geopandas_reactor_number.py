df16ddd28ad9f340a954d67f8a51b07a60998cdf
johannesschabbauer <johscha123@gmail.com>
2021-01-03 19:29:21 +0100

    Changed Colorscheme and use merge dataframe for 'plot_geopandas_reactor_number.py'

--- scripts/plot_geopandas_reactor_number.py
+++ scripts/plot_geopandas_reactor_number.py
@@ -8,10 +8,9 @@ import matplotlib.pyplot as plt
 import geopandas
 import geoplot
 import mapclassify
-#from mpl_toolkits.axes_grid1 import make_axes_locatable
+import math
 import country_converter
 
-import load_data_political
 
 # initialize CountryConverter as cc and disable warnings
 cc = country_converter.CountryConverter()
@@ -23,51 +22,45 @@ world = world.drop(world.index[159])
 world['iso_a3'] = cc.convert(world['name'].to_list(), to='ISO3')
 
 # get data of nuclear reactors
-reactors = load_data_political.load_reactor_numbers()
-
-def reactors_year(year):
-    # function to get reactor numbers per year 
-    return reactors.loc[year]
-#
-reactors_quot_1980_2020 = reactors_year(1980).divide(reactors_year(2020), fill_value=0)
+data = pd.read_csv('../data/data_merged/data.csv').set_index(['year','country'])
+data = data[['built_reactors','shutdown_reactors','operating_reactors']]
 
 # merge with geographical data
 def merge_year(year):
-    merge = pd.merge(world,reactors_year(year), left_on='iso_a3', right_on='country', how='left')
+    merge = pd.merge(world,data.loc[year].replace(0,np.nan), left_on='iso_a3', right_on='country', how='left')
     return merge
 
 # create plots for given years
-years = np.arange(1980,2021,5)#[1970,1980,1990,2000,2010,2020]
-fig, ax = plt.subplots(len(years)//3, 3, constrained_layout=True, 
+years = [1980,1985,1990,1995,2000,2005,2010,2015,2018]
+fig, ax = plt.subplots(math.ceil(len(years)/3), 3, constrained_layout=True, 
                         sharex=True, sharey=True, 
                         subplot_kw=dict(aspect='equal'))
 # flatten axes for iteration
 ax = ax.ravel()
-# scaling of colorbar
-#divider = make_axes_locatable(ax)
-#cax = divider.append_axes("right", size="2%", pad=-0.1)
 
 for i,year in enumerate(years):
     merge_year(year).plot(
     	column = 'operating_reactors',
     	ax=ax[i],
-        #cax=cax,
     	missing_kwds={"color": "lightgrey", "label": "No nuclear power plants"}, # missing values in grey
-        cmap='plasma', # scheme of colormap
-        vmax=reactors.query("year in @years")['operating_reactors'].max() # set maximum of legend (would be different for every subplot)
+        cmap='Reds', # scheme of colormap
+        vmin=0,
+        vmax=data.query("year in @years")['operating_reactors'].max() # set maximum of legend (would be different for every subplot)
     	)
     ax[i].set_title(f'{year}')
     ax[i].axis('off')
 patch_col = ax[0].collections[0]
 fig.colorbar(patch_col, ax=ax, shrink=0.5)
-fig.suptitle('Operating nuclear reactors')
+fig.suptitle('Operating nuclear reactors per year (Grey color if zero)')
 #plt.show()
 
 # second plot for evolution of total reactors worldwide
-data = reactors.sum(axis=0, level='year').sort_index()
-data.plot()
+data_sum = data.sum(axis=0, level='year').sort_index()
+data_sum.plot()
 plt.legend(loc='best')
 plt.title('Total Nuclear Reactors Worldwide')
+plt.xlim(data_sum.index.min(),data_sum.index.max())
+plt.grid()
 plt.show()
 
 
