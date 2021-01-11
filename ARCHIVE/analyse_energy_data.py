fca2d83f4f26009ad7840d21a485dffe2266acae
Josef Glas <e8606876@student.tuwien.ac.at>
2021-01-11 01:19:50 +0100

    minor change

--- ARCHIVE/analyse_energy_data.py
+++ ARCHIVE/analyse_energy_data.py
@@ -4,13 +4,13 @@ python module to analyse energy data
 
 import numpy as np
 import pandas as pd
-import load_energy1_data as led
+import load_energy2_data as led
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 # main function for testing
 if __name__ == '__main__':
-    df = led.load_bp_data()
+    df = led.load_u
 
     sns.set(font_scale = 0.4)
     # visualize pattern of all missing falues
